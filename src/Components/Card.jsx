ae88060710fb6a889c06bb86c2f0ca820d664bde
Peter Choi <ptrchoi@gmail.com>
2020-06-26 19:14:25 -0500

    added hide className to images with no source-url

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,9 +10,13 @@ function Card(props) {
 
 	let renderCards = () => {
 		return cards.map((card) => {
+			let imageClassList = 'card-image';
+			if (card.imgSrc === '') {
+				imageClassList = imageClassList + ' hide-image';
+			}
 			return (
 				<div key={card.id} className="card-wrapper">
-					<img className="card-image" src={card.imgSrc} alt="title image" />
+					<img className={imageClassList} src={card.imgSrc} alt="title image" />
 					<h2>{card.title}</h2>
 					<p>{card.url}</p>
 					<a href={card.url} target="_blank" />
