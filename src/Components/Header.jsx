0ce57b55381bd21c38f1ed469401604258c4f4a7
Peter Choi <ptrchoi@gmail.com>
2020-07-06 19:14:02 -0500

    added app logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+const cardImg = require('../images/cardImage.png');
+
 function Header(props) {
 	return (
 		<div className="header-wrapper">
 			<h1>Top Tweets Wiki</h1>
+			<img className="logoImg" src={cardImg} />
 		</div>
 	);
 }
